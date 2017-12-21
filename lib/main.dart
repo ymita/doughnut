a54666c5dd88f43909f53dbda9ac4ceb651c96d1
Yuki Mita <YMita@infragistics.com>
2017-12-22 08:50:02 +0900

    removed unnecessary code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
+void main() {
+  runApp(new MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -43,67 +45,82 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+  var idx = 0;
+  final TextEditingController _controller = new TextEditingController();
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return new Scaffold(
+      
       appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
+        title: new Text('doughnut'),
       ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+      
+      body:
+      idx == 0 ?
+      new Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
+            new TextField(
+              controller: _controller,
+              decoration: new InputDecoration(
+                hintText: 'Type text here',
+              ),
             ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            new ListTile(
+              leading: const Icon(Icons.assignment_ind),
+              title: const Text('What is this?'),
+              trailing: new IconButton(
+                icon: new Icon(Icons.volume_up),
+                tooltip: 'Listen to learn!',
+                onPressed: () { setState(() { var a = 1.1; }); },
+              ),
+            ),
+            new ListTile(
+              leading: const Icon(Icons.assignment_ind),
+              title: const Text('How are you?'),
+              trailing: new IconButton(
+                icon: new Icon(Icons.volume_up),
+                tooltip: 'Listen to learn!',
+                onPressed: () { setState(() { var a = 1.1; }); },
+              ),
+            ),
+            new ListTile(
+              leading: const Icon(Icons.assignment_ind),
+              title: const Text('What is your name?'),
+              trailing: new IconButton(
+                icon: new Icon(Icons.volume_up),
+                tooltip: 'Listen to learn!',
+                onPressed: () { setState(() { var a = 1.1; }); },
+              ),
+            ),
+            new ListTile(
+              leading: const Icon(Icons.assignment_ind),
+              title: const Text('My name is Bob Smith.'),
+              trailing: new IconButton(
+                icon: new Icon(Icons.volume_up),
+                tooltip: 'Listen to learn!',
+                onPressed: () { setState(() { var a = 1.1; }); },
+              ),
             ),
           ],
-        ),
+        )
+      :
+      new Text("Right"),
+      bottomNavigationBar: new BottomNavigationBar(
+        currentIndex: idx,
+        onTap: (int idx) { setState((){ this.idx = idx; }); },
+        items: <BottomNavigationBarItem>[
+          new BottomNavigationBarItem(
+            icon: new Icon(Icons.home),
+            title: new Text("Left"),
+          ),
+          new BottomNavigationBarItem(
+            icon: new Icon(Icons.search),
+            title: new Text("Right"),
+          ),
+        ],
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      
     );
   }
 }
