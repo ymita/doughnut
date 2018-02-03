2c4535be0b121301e8efe35541183453b010c94d
Yuki Mita <YMita@infragistics.com>
2018-02-03 12:18:27 +0900

    feat(search): fetch example sentences from web service

--- lib/search/search.dart
+++ lib/search/search.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 import 'dart:async' show Future;
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:xml/xml.dart' as xml;
 
 class Search extends StatefulWidget {
   @override
@@ -16,33 +18,64 @@ class SearchState extends State<Search> {
   List<SearchResult> _searchResults = [];
 
   Future<List<SearchResult>> fetchWords(String query) async {
-
     final firstChar = query[0];
     final loadedFile = await rootBundle.loadString('assets/$firstChar.json');
     final json = new JsonDecoder().convert(loadedFile);
     final list = new List<SearchResult>();
 
     (json as Map<String, dynamic>).forEach((key, value) {
-      if(key.toLowerCase().startsWith(query)){
+      if (key.toLowerCase().startsWith(query)) {
         list.add(new SearchResult(
-          word: value["word"],
-          englishSentence: value["wordset_id"])
-        );
+            word: value["word"], englishSentence: value["wordset_id"]));
       }
     });
 
     return list;
   }
 
+  Future<List<SearchResult>> fetchSentences(String query) async {
+    //sentences returned to _search
+    final list = new List<SearchResult>();
+    //Service URL
+    var url = 'https://www.dictionaryapi.com/api/v1/references/learners/xml/$query?key=d89a06d2-eff1-4f07-b08f-869850bc8b57';
+    var httpClient = new HttpClient();
+
+    try {
+      var request = await httpClient.getUrl(Uri.parse(url));
+      var response = await request.close();
+      if (response.statusCode == HttpStatus.OK) {
+        
+        //Transform response data into XmlDocument
+        var content = await response.transform(UTF8.decoder).join();
+        var document = xml.parse(content);
+        
+        //retrieve example sentences for the word 
+        var sentences = document.findAllElements('vi');
+        
+        //add the sentences to the list
+        sentences.forEach((value){
+          list.add(new SearchResult(
+            word: value.toString(), englishSentence: value.toString()));
+        });
+      } else {
+        print("error : ${response.statusCode}");
+      }
+    } catch (exception) {
+      print("failed");
+    }
+
+    return list;
+  }
+
   void _search(String inputValue) {
-    if (inputValue.isEmpty) { return; }
+    if (inputValue.isEmpty) {
+      return;
+    }
 
     final String query = inputValue.toLowerCase();
-    this.fetchWords(query).then((_list) {
+    this.fetchSentences(query).then((_list) {
       setState(() {
-        _searchResults = _list.where((item) {
-          return item.word.toLowerCase().startsWith(query) ? true : false;
-        }).toList();
+        _searchResults = _list;
       });
     });
   }
@@ -52,10 +85,9 @@ class SearchState extends State<Search> {
     return new Column(
       children: <Widget>[
         new TextField(
-          controller: _controller,
-          decoration: new InputDecoration(hintText: 'Type word'),
-          onSubmitted: _search
-        ),
+            controller: _controller,
+            decoration: new InputDecoration(hintText: 'Type word'),
+            onSubmitted: _search),
         new Expanded(
           child: new ListView(
             children: _searchResults.map((SearchResult searchResult) {
@@ -63,12 +95,11 @@ class SearchState extends State<Search> {
                 leading: const Icon(Icons.assignment_ind),
                 title: new Text(searchResult.word),
                 trailing: new IconButton(
-                  icon: new Icon(Icons.volume_up),
-                  tooltip: searchResult.englishSentence,
-                  onPressed: () {
-                    print(searchResult.japaneseSentence);
-                  }
-                ),
+                    icon: new Icon(Icons.volume_up),
+                    tooltip: searchResult.englishSentence,
+                    onPressed: () {
+                      print(searchResult.japaneseSentence);
+                    }),
               );
             }).toList(),
           ),
